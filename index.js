a6c60c1e13e0c72e4e906c155240e3a6e04d4b75
ykey <ykey@ykey.com>
2020-10-21 14:39:22 +0900

    babel add

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("TEST");
+
+import express from "express";
+const app = express();
+const PORT = 4000;
+
+const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
+const handleHome = (req, res) => {
+    console.log(req);
+    return res.send("Hello from HOMe");
+};
+const handleProfile = (req, res) => {
+    console.log(req);
+    return res.send("Hello from Profile");
+};
+app.get("/",handleHome);
+app.get('/profile',handleProfile)
+app.listen(PORT,handleListening);
